7f56ea58f1b01a00b4101fdd23355b8075645f98
Matias-sh <devmatiasbritez@gmail.com>
2024-10-07 12:40:37 -0300

    button sign up finished

--- app/src/main/java/com/example/cursofirebaselite/presentation/initial/InitialScreen.kt
+++ app/src/main/java/com/example/cursofirebaselite/presentation/initial/InitialScreen.kt
@@ -5,14 +5,27 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cursofirebaselite.R
 import com.example.cursofirebaselite.ui.theme.Black
 import com.example.cursofirebaselite.ui.theme.Gray
+import com.example.cursofirebaselite.ui.theme.Green
 
 @Preview
 @Composable
@@ -20,12 +33,40 @@ fun InitialScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Brush.verticalGradient(listOf(Gray, Black), startY = 0f, endY = 1000f))
+            .background(Brush.verticalGradient(listOf(Gray, Black), startY = 0f, endY = 1000f)),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.weight(1f))
-        Image(painter = painterResource(id = R.drawable.spotify), contentDescription = "")
+        Image(
+            painter = painterResource(id = R.drawable.spotify),
+            contentDescription = "",
+            modifier = Modifier.clip(
+                CircleShape
+            )
+        )
         Spacer(modifier = Modifier.weight(1f))
+        Text(
+            "Millions of Songs.",
+            color = Color.White,
+            fontSize = 38.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            "Free on Spotify.",
+            color = Color.White,
+            fontSize = 38.sp,
+            fontWeight = FontWeight.Bold
+        )
         Spacer(modifier = Modifier.weight(1f))
+        Button(
+            onClick = { /*TODO*/ }, modifier = Modifier
+                .fillMaxWidth()
+                .padding(32.dp), colors = ButtonDefaults.buttonColors(containerColor = Green)
+        ) {
+            Text(text = "Sign up free", color = Color.Black)
+
+
+        }
 
     }
 }
